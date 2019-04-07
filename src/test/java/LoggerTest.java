bd7baa45f9479ae524975a213591e788cc47daa1
AlexeySirant <lexsirant@gmail.com>
2019-04-07 16:09:03 +0300

    3th

---
+++ src/test/java/LoggerTest.java
@@ -0,0 +1,16 @@
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+// import java.util.logging.Logger;
+
+// public class LoggerTest {
+
+/*
+    static LoggerTest{
+
+        static Logger log = LogManager.getLogger(
+                LoggerTest.class.getName()
+        );
+    }
+}
+*/
